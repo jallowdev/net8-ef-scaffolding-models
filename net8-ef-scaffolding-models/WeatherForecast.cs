ab6ccb079516825ad1ad057867b3931fea54b459
DIALLO <ibrahima.diallo@diotali.com>
2024-03-10 00:19:29 +0000

    initial commit

---
+++ net8-ef-scaffolding-models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace net8_ef_scaffolding_models
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
